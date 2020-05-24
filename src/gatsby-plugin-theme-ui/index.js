7cf3e3fd3e95def1ddcd0fab1c685b592eca2d8f
Deeptish <deeptish.design@gmail.com>
2020-05-24 21:11:42 +0530

    Header height position

--- src/gatsby-plugin-theme-ui/index.js
+++ src/gatsby-plugin-theme-ui/index.js
@@ -7,9 +7,9 @@ export default merge(baseTheme, {
     headerHeight: "60px", // Sets the height of the header on screens before the first breakpoint, 767px and below by default
     headerHeightTablet: "80px", // Sets the height of the header after the first breakpoint, 768px to 1023px by default
     headerHeightLaptop: "80px", // Sets the height of the header after the second breakpoint, 1024px and up by default
-    mainMarginTop: "200px", // Makes sure the content comes below the fixed header, should be the same values as headerHeight
-    mainMarginTopTablet: "200px", // Makes sure the content comes below the fixed header, should be the same values as headerHeight
-    mainMarginTopLaptop: "200px", // Makes sure the content comes below the fixed header, should be the same values as headerHeight
+    mainMarginTop: "60px", // Makes sure the content comes below the fixed header, should be the same values as headerHeight
+    mainMarginTopTablet: "80px", // Makes sure the content comes below the fixed header, should be the same values as headerHeight
+    mainMarginTopLaptop: "80px", // Makes sure the content comes below the fixed header, should be the same values as headerHeight
   },
   catalystOptions: {
     headerPosition: "fixed", //Fix the header for single page navigation ease
